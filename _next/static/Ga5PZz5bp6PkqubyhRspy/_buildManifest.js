72e52b3114a2372214e777ea44a8f5649c818aed
unknown <annybishnoi1@gmail.com>
2026-05-13 18:06:39 +0530

    Updates

--- _next/static/Ga5PZz5bp6PkqubyhRspy/_buildManifest.js
+++ _next/static/Ga5PZz5bp6PkqubyhRspy/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/developer-portfolio//_next/:path+",
+        "destination": "/developer-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
